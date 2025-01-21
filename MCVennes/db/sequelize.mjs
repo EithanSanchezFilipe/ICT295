beff8575376e3af3cda2a754cc7623dcbe4b2a7c
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-01-21 13:59:21 +0100

    feat : models orm

--- MCVennes/db/sequelize.mjs
+++ MCVennes/db/sequelize.mjs
@@ -14,4 +14,24 @@ const sequelize = new Sequelize(
   }
 );
 
+import { products } from './mock-product.mjs';
+// Le modèle product
+const Product = ProductModel(sequelize, DataTypes);
+let initDb = () => {
+  return sequelize
+    .sync({ force: true }) // Force la synchro => donc supprime les données également
+    .then((_) => {
+      importProducts();
+      console.log('La base de données db_products a bien été synchronisée');
+    });
+};
+const importProducts = () => {
+  // import tous les produits présents dans le fichier db/mock-product
+  products.map((product) => {
+    Product.create({
+      name: product.name,
+      price: product.price,
+    }).then((product) => console.log(product.toJSON()));
+  });
+};
 export { sequelize, initDb, Product };
